4d1b469e150a3a3caba8b13e9a0db033922fbd61
Animesh Dey <animesh.dey986@gmail.com>
2022-05-06 16:36:08 +0530

    ok

--- src/Models/internModel.js
+++ src/Models/internModel.js
@@ -5,6 +5,7 @@ let validateEmail = function(email) {
     let emailRegex = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
     return emailRegex.test(email)
 }
+
 let validateMobile = function(mobile){
     let mobileRegex = /^[0-9]{10}$/
     return mobileRegex.test(mobile)
