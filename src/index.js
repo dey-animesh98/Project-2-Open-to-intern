6555e8d678ae5988e02f62885b9e95ad20742a98
Animesh Dey <animesh.dey986@gmail.com>
2022-05-06 10:58:37 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const route = require("./Routes/routes") 
+const route = require("./Routes/routes")
 const { default: mongoose } = require("mongoose")
 const app = express()
+const port = 5500
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect("mongodb+srv://animesh-dey98:9I9JRLwql3bINqUX@cluster0.vhmqo.mongodb.net/groupXDatabase",{
+mongoose.connect("mongodb+srv://animesh-dey98:9I9JRLwql3bINqUX@cluster0.vhmqo.mongodb.net/groupXDatabase", {
     useNewUrlParser: true
 })
 
- .then(() => console.log("MongoDB is Connected."))
- .catch(err => console.log(err))
+    .then(() => console.log("MongoDB is Connected."))
+    .catch(err => console.log(err))
 
- app.use('/', route)
+app.use('/', route)
 
- app.listen(process.env.PORT || 3000, function (){
-     console.log("Express is running on port " + (process.env.PORT || 3000))
- })
+app.listen(process.env.PORT || port, function () {
+    console.log("Express is running on port " + (process.env.PORT || port))
+})
